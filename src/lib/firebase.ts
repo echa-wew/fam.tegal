fdb99de028698b77f3c32bc5b08a50844f9abd42
Raden <prasetya.inc@gmail.com>
2026-09-06 22:33:27 +0700

    Refactor Firebase initialization code

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBEi9k8cT_wHY5wsWJNR5EFMAlk-5nGj7g",
   authDomain: "silsilah-fam-tegal.firebaseapp.com",
@@ -16,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-SBWDKMQK9Y"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
